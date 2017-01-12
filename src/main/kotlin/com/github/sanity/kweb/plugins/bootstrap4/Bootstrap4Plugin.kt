74c805e4ea8cc39a435e06c3f8d82dca2077b4c2
Gregory Tod <git@gtod.net>
2017-01-12 20:06:51 +1100

    Remove jQuery cruft from bootstrap plugin

--- src/main/kotlin/com/github/sanity/kweb/plugins/bootstrap4/Bootstrap4Plugin.kt
+++ src/main/kotlin/com/github/sanity/kweb/plugins/bootstrap4/Bootstrap4Plugin.kt
@@ -1,9 +1,6 @@
 package com.github.sanity.kweb.plugins.bootstrap4
 
-import com.github.sanity.kweb.RootReceiver
 import com.github.sanity.kweb.plugins.KWebPlugin
-import com.github.sanity.kweb.plugins.jqueryCore.JQueryCorePlugin
-import com.github.sanity.kweb.toJson
 
 /**
  * Created by ian on 1/10/17.
@@ -20,22 +17,5 @@ class Bootstrap4Plugin : KWebPlugin {
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
         """.trimIndent()
         )
-    }
-}
-
-// A convenience value
-val bootstrap4 = JQueryCorePlugin()
-
-// Support for $(...), since Kotlin doesn't allow methods called '$' (which is probably a good thing)
-// I just use jquery()
-fun RootReceiver.jquery(selector: String): JQueryReceiver {
-    require(JQueryCorePlugin::class)
-    return JQueryReceiver(this, "$(${selector.toJson()})")
-}
-
-// And here we can implement all of the useful JQuery functions
-class JQueryReceiver(private val rootReceiver: RootReceiver, private val js: String) {
-    fun remove() {
-        rootReceiver.execute(js + ".remove();")
     }
 }
