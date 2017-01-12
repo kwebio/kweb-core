192cdba02ef7ee4c40b8220fd9f3aaa61dfb4495
Ian Clarke <github@ian.33mail.com>
2017-01-12 09:31:20 -0600

    checkpoint

--- src/test/kotlin/com/github/sanity/kweb/PhantomJSSpec.kt
+++ src/test/kotlin/com/github/sanity/kweb/PhantomJSSpec.kt
@@ -4,7 +4,7 @@ import com.moodysalem.phantomjs.wrapper.PhantomJS
 import io.kotlintest.specs.FreeSpec
 import kotlinx.coroutines.async
 import kotlinx.coroutines.await
-import kotlin.concurrent.thread
+import java.util.concurrent.CompletableFuture
 
 /**
  * Created by ian on 1/10/17.
@@ -12,36 +12,62 @@ import kotlin.concurrent.thread
 class PhantomJSSpec : FreeSpec() {
     init {
         "Should be able to set and retrieve an attribute" {
-            thread {
                 KWeb(7324) {
-                    doc.body.apply {
-                        async {
-                            val h1 = h1("testing").class_("testclass")
-                            println("Read class: ${h1.read.class_.await()}")
+                    println("Hello")
+                    with(doc.body) {
+                        println("Hello2")
+                        val future: CompletableFuture<Unit> = async {
+                            //                            throw RuntimeException("Fake exception")
+                            println("Writing H1")
+                            val h1 = h1("testing").setAttribute("data-test", "abacus")
+                            println("H1 written")
+                            println("Read " + h1.read.attribute("data-test").await())
+                            println("after read")
                             execute("console.log('Kweb works');")
-                        }.get()
+                        }
+                        // The function is delayed, ties up the receiver thread in KWeb.handleWS()
+                        Thread.sleep(10000)
+                        /*  try {
+                          future.get()  // this seems to prevent .await() from working
+                      } catch (e : ExecutionException) {
+                          throw e.cause!!
+                      }*/
                     }
                 }
-            }
-            Thread.sleep(500)
+
             val script =
                     """
-var page = require('webpage').create();
-page.open('http://127.0.0.1:7324', function(status) {
-  console.log("Status: " + status);
-  if(status === "success") {
-    waitFor(function() {
-        return page.evaluate(function() {
-            return $(".testclass").is(":visible");
-        });
-    }, function() {
-        console.log("<h1> rendered");
-        phantom.exit();
+phantom.onError = function(msg, trace) {
+  var msgStack = ['PHANTOM ERROR: ' + msg];
+  if (trace && trace.length) {
+    msgStack.push('TRACE:');
+    trace.forEach(function(t) {
+      msgStack.push(' -> ' + (t.file || t.sourceURL) + ': ' + t.line + (t.function ? ' (in function ' + t.function +')' : ''));
     });
   }
+  console.error(msgStack.join('\n'));
+  phantom.exit(1);
+};
+var page = require('webpage').create();
+page.onConsoleMessage = function(msg, lineNum, sourceId) {
+  console.log('CONSOLE: ' + msg);
+};
+page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
+  page.open('http://127.0.0.1:7324', function(status) {
+    if (status === "success") {
+       window.setTimeout(function () {
+            phantom.exit();
+        }, 15000);
+      //waitFor("$('.testclass').is(':visible')", "console.log('<h1> rendered');");
+    }
+  })
 });
+
                 """.trimIndent()
-            PhantomJS.exec(script.byteInputStream())
+            val result = PhantomJS.exec(script.byteInputStream())
+            println(result.stdOut)
+
         }
+
     }
 }
