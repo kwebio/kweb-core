544ef5d33f5322c021fc9f12f8c3c730ba5e3e5a
Gregory Tod <git@gtod.net>
2017-01-12 20:04:23 +1100

    Bootstrap wants a viewport meta and tether.js.  See http://v4-alpha.getbootstrap.com/getting-started/introduction/

--- src/main/kotlin/com/github/sanity/kweb/plugins/bootstrap4/Bootstrap4Plugin.kt
+++ src/main/kotlin/com/github/sanity/kweb/plugins/bootstrap4/Bootstrap4Plugin.kt
@@ -13,7 +13,10 @@ class Bootstrap4Plugin : KWebPlugin {
     override fun decorate(startHead: StringBuilder, endHead: StringBuilder) {
         // From https://v4-alpha.getbootstrap.com/getting-started/download/#source-files
         startHead.appendln("""
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
         """.trimIndent()
         )
