da2d9daa0c79c4b8110d3f94d2f1dbb7c5b72297
Ian Clarke <github@ian.33mail.com>
2017-01-13 14:25:55 -0600

    comments

--- src/test/kotlin/com/github/sanity/kweb/PhantomJSSpec.kt
+++ src/test/kotlin/com/github/sanity/kweb/PhantomJSSpec.kt
@@ -11,18 +11,19 @@ import java.util.concurrent.CompletableFuture
 import kotlin.concurrent.thread
 
 /**
- * This works, but is very limited.
+ * This works, but is very limited and clunky.  Really need a way to test all of the JavaScript stuff.
  */
 class PhantomJSSpec : FreeSpec() {
     init {
-        val exceptionFuture = CompletableFuture<Throwable>()
+        val exceptionFuture = CompletableFuture<Unit>()
 
         "should be able to set and read an attribute" {
             KWeb(7324) {
                     with(doc.body) {
                         async {
                             val h1 = h1("testing").setAttribute("data-test", "abacus")
-                            h1.read.attribute("data-test").await() shouldBe "abacuss"
+                            h1.read.attribute("data-test").await() shouldBe "abacus"
+                            exceptionFuture.complete(Unit)
                         }.exceptionally { exceptionFuture.completeExceptionally(it) }
                     }
             }
