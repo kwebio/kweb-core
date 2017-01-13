8af6215955a12ad243df5ec5b3dc09aeae32f02f
Gregory Tod <git@gtod.net>
2017-01-13 19:27:14 +1100

    Full css wrapper for bootstrap demo

--- src/main/kotlin/com/github/sanity/kweb/demos/bootstrap4/bootstrap4.kt
+++ src/main/kotlin/com/github/sanity/kweb/demos/bootstrap4/bootstrap4.kt
@@ -12,6 +12,21 @@ From http://v4-alpha.getbootstrap.com/components/forms/#textual-inputs
     <input class="form-control" type="text" value="Artisanal kale" id="example-text-input">
   </div>
 </div>
+
+<div class="bd-pageheader text-center text-sm-left">
+  <div class="container">
+    <h1>{{ page.title }}</h1>
+    <p class="lead">
+      Quickly get a project started with any of our examples ranging from using parts of the framework to custom components and layouts.
+    </p>
+    {% include ads.html %}
+  </div>
+</div>
+
+<div class="container bd-content">
+  {{ content }}
+</div>
+
  */
 
 fun main(args: Array<String>) {
@@ -19,7 +34,19 @@ fun main(args: Array<String>) {
     KWeb(port = 8091, plugins = listOf(bootstrap4)) {
         doc.body.apply {
             createElement("div").apply {
-                setAttribute("class", "container")
+                setAttribute("class", "bd-pageheader text-center text-sm-left")
+
+                createElement("div").apply {
+                    setAttribute("class", "container")
+
+                    h1("KWeb Bootstrap4 demo")
+
+                    p("The following is a simple text field which does nothing")
+                }
+            }
+
+            createElement("div").apply {
+                setAttribute("class", "container bd-content")
 
                 createElement("div").apply {
                     setAttribute("class", "form-group row")
