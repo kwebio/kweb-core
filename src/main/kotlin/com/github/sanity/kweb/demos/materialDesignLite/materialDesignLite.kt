bfc37f82c66f63906ac005672fca9d6a1421726f
Ian Clarke <github@ian.33mail.com>
2017-01-14 17:29:37 -0600

    remove comment

--- src/main/kotlin/com/github/sanity/kweb/demos/materialDesignLite/materialDesignLite.kt
+++ src/main/kotlin/com/github/sanity/kweb/demos/materialDesignLite/materialDesignLite.kt
@@ -8,32 +8,6 @@ import com.github.sanity.kweb.plugins.materialdesignlite.materialDesignLite
 import com.github.sanity.kweb.plugins.materialdesignlite.mdl
 import com.github.sanity.kweb.plugins.materialdesignlite.typography
 
-/*
-From http://v4-alpha.getbootstrap.com/components/forms/#textual-inputs
-
-<div class="form-group row">
-  <label for="example-setText-input" class="col-2 col-form-label">Text</label>
-  <div class="col-10">
-    <input class="form-control" type="setText" value="Artisanal kale" id="example-setText-input">
-  </div>
-</div>
-
-<div class="bd-pageheader setText-center setText-sm-left">
-  <div class="container">
-    <h1>{{ page.title }}</h1>
-    <p class="lead">
-      Quickly getString a project started with any of our examples ranging from using parts of the framework to custom components and layouts.
-    </p>
-    {% include ads.html %}
-  </div>
-</div>
-
-<div class="container bd-content">
-  {{ content }}
-</div>
-
- */
-
 fun main(args: Array<String>) {
     println("Visit http://127.0.0.1:8091/ in your browser...")
     KWeb(port = 8091, plugins = listOf(materialDesignLite)) {
