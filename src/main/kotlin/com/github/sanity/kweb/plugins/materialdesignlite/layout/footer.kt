84511f3aa2b7293bfaee5ad15c7bd384d001dfda
Ian Clarke <github@ian.33mail.com>
2017-01-22 17:53:05 -0600

    bump version

--- src/main/kotlin/com/github/sanity/kweb/plugins/materialdesignlite/layout/footer.kt
+++ src/main/kotlin/com/github/sanity/kweb/plugins/materialdesignlite/layout/footer.kt
@@ -26,9 +26,6 @@ class MiniFooterSection(wrapped: DivElement) : DivElement(wrapped) {
 
     // TODO: User needs to be able to specify their own social button css classes, see:
     // TODO: https://github.com/google/material-design-lite/blob/88872e672e41c56af0a78a35b34373b8c4a8c49d/docs/_assets/main.css#L533
+    // TODO: This might need to wait for a css DSL
     fun socialButton(attributes: Map<String, Any> = attr) = button(attributes = attributes.classes("mdl-mini-footer__social-btn"))
-}
-
-enum class MiniFooterSide {
-    left, right
 }
