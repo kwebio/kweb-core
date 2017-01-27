001cc22ded33111c2d11d0335042fc177911b5a1
Ian Clarke <github@ian.33mail.com>
2017-01-27 08:47:55 -0600

    rearrange methods in mdl demo

--- src/main/kotlin/com/github/sanity/kweb/demos/materialDesignLite/materialDesignLite.kt
+++ src/main/kotlin/com/github/sanity/kweb/demos/materialDesignLite/materialDesignLite.kt
@@ -28,35 +28,29 @@ fun main(args: Array<String>) {
     }
 }
 
-private fun LayoutElement.addFooter() {
-    mdl.miniFooter().apply {
-        addLeftSection()
-        addRightSection()
-    }
-}
-
-private fun MiniFooterElement.addRightSection() {
-    rightSection().apply {
-        linkList().apply {
-            li().a().setText("Footer link")
-            li().a().setText("Footer link")
-            li().a().setText("Footer link")
-            li().a().setText("Footer link")
+private fun LayoutElement.addHeader() {
+    header().apply {
+        row().apply {
+            title().setText("Title")
+            spacer()
+            navigation().apply {
+                navLink().setText("First header link")
+                navLink().setText("Second header link")
+                navLink().setText("Third header link")
+                navLink().setText("Fourth header link")
+            }
         }
     }
 }
 
-private fun MiniFooterElement.addLeftSection() {
-    leftSection().apply {
-        logo()
-        linkList().apply {
-            li().a().setText("Footer link")
-                    .on.click {
-                println("Footer link clicked")
-            }
-            li().a().setText("Footer link")
-            li().a().setText("Footer link")
-            li().a().setText("Footer link")
+private fun LayoutElement.addDrawer() {
+    drawer().apply {
+        title().setText("Title")
+        navigation().apply {
+            navLink().setText("First drawer link")
+            navLink().setText("Second drawer link")
+            navLink().setText("Third drawer link")
+            navLink().setText("Fourth drawer link")
         }
     }
 }
@@ -71,27 +65,19 @@ private fun LayoutElement.addContent() {
     }
 }
 
-private fun TabsElement.addListPanel() {
-    panel("List").apply {
-        grid().div(offsetBefore = 2, width = 8).apply {
-            list().apply {
-                addActorItem("Bryan Cranston", "Bryan Cranston played the role of Walter in Breaking Bad. He is also known for playing Hal in Malcom in the Middle.")
-                addActorItem("Aaron Paul", "Aaron Paul played the role of Jesse in Breaking Bad. He also featured in the \"Need For Speed\" Movie.")
-                addActorItem("Bob Odenkirk", "Bob Odinkrik played the role of Saul in Breaking Bad. Due to public fondness for the character, Bob stars in his own show now, called \"Better Call Saul\".")
-            }
-        }
-    }
-}
+private fun TabsElement.addTableFromObjectsPanel() {
+    panel("Table from Objects", isActive = true).apply {
+        grid().div(offsetBefore = 5, width = 2).apply {
+            table().apply {
+                data class Row(@MDLTableHeaderName("First Name") val firstName: String, val address: String)
 
-private fun MDLUlElement.addActorItem(name: String, bio: String) {
-    item(type = ListItemType.threeLine).apply {
-        primaryContent().apply {
-            avatar().setText("person")
-            span().setText(name)
-            body().setText(bio)
-        }
-        secondaryContent().apply {
-            secondaryAction().icon().setText("star")
+                val data = listOf(Row("Ian", "Austin"), Row("Fred", "Houston"))
+                fromDataObjectList(data, propertyOrder = listOf(Row::firstName, Row::address)) { obj, td ->
+                    td.on.click {
+                        println(obj.firstName)
+                    }
+                }
+            }
         }
     }
 }
@@ -124,46 +110,62 @@ private fun TabsElement.addManualTablePanel() {
     }
 }
 
-private fun LayoutElement.addDrawer() {
-    drawer().apply {
-        title().setText("Title")
-        navigation().apply {
-            navLink().setText("First drawer link")
-            navLink().setText("Second drawer link")
-            navLink().setText("Third drawer link")
-            navLink().setText("Fourth drawer link")
-        }
-    }
-}
-
-private fun LayoutElement.addHeader() {
-    header().apply {
-        row().apply {
-            title().setText("Title")
-            spacer()
-            navigation().apply {
-                navLink().setText("First header link")
-                navLink().setText("Second header link")
-                navLink().setText("Third header link")
-                navLink().setText("Fourth header link")
+private fun TabsElement.addListPanel() {
+    panel("List").apply {
+        grid().div(offsetBefore = 2, width = 8).apply {
+            list().apply {
+                addActorItem("Bryan Cranston", "Bryan Cranston played the role of Walter in Breaking Bad. He is also known for playing Hal in Malcom in the Middle.")
+                addActorItem("Aaron Paul", "Aaron Paul played the role of Jesse in Breaking Bad. He also featured in the \"Need For Speed\" Movie.")
+                addActorItem("Bob Odenkirk", "Bob Odinkrik played the role of Saul in Breaking Bad. Due to public fondness for the character, Bob stars in his own show now, called \"Better Call Saul\".")
             }
         }
     }
 }
 
-private fun TabsElement.addTableFromObjectsPanel() {
-    panel("Table from Objects", isActive = true).apply {
-        grid().div(offsetBefore = 5, width = 2).apply {
-            table().apply {
-                data class Row(@MDLTableHeaderName("First Name") val firstName: String, val address: String)
-
-                val data = listOf(Row("Ian", "Austin"), Row("Fred", "Houston"))
-                fromDataObjectList(data, propertyOrder = listOf(Row::firstName, Row::address)) { obj, td ->
-                    td.on.click {
-                        println(obj.firstName)
-                    }
-                }
-            }
+private fun MDLUlElement.addActorItem(name: String, bio: String) {
+    item(type = ListItemType.threeLine).apply {
+        primaryContent().apply {
+            avatar().setText("person")
+            span().setText(name)
+            body().setText(bio)
+        }
+        secondaryContent().apply {
+            secondaryAction().icon().setText("star")
         }
     }
 }
+
+private fun LayoutElement.addFooter() {
+    mdl.miniFooter().apply {
+        addLeftSection()
+        addRightSection()
+    }
+}
+
+private fun MiniFooterElement.addLeftSection() {
+    leftSection().apply {
+        logo()
+        linkList().apply {
+            li().a().setText("Footer link")
+                    .on.click {
+                println("Footer link clicked")
+            }
+            li().a().setText("Footer link")
+            li().a().setText("Footer link")
+            li().a().setText("Footer link")
+        }
+    }
+}
+
+private fun MiniFooterElement.addRightSection() {
+    rightSection().apply {
+        linkList().apply {
+            li().a().setText("Footer link")
+            li().a().setText("Footer link")
+            li().a().setText("Footer link")
+            li().a().setText("Footer link")
+        }
+    }
+}
+
+
