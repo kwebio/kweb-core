6c3ff0dc895595f8bb4f91d1abb61850bb55de22
Ian Clarke <github@ian.33mail.com>
2017-01-27 13:53:07 -0600

    clean up DSL a bit

--- src/main/kotlin/com/github/sanity/kweb/plugins/materialdesignlite/list/list.kt
+++ src/main/kotlin/com/github/sanity/kweb/plugins/materialdesignlite/list/list.kt
@@ -12,8 +12,9 @@ import com.github.sanity.kweb.plugins.materialdesignlite.MDLElement
 
 fun MDLElement.list(attributes: Map<String, Any> = attr) = MDLUlElement(ul(attributes.classes("mdl-list")))
 
-class MDLUlElement(wrapped: ULElement) : ULElement(wrapped) {
-    fun item(attributes: Map<String, Any> = attr, type: ListItemType = ListItemType.oneLine) = MDLItemElement(super.li(attributes
+class MDLUlElement(wrapped: ULElement) : MDLElement(wrapped) {
+    fun item(attributes: Map<String, Any> = attr, type: ListItemType = ListItemType.oneLine)
+            = MDLItemElement(createElement("li", attributes
             .classes("mdl-list__item")
             .classes("mdl-list__item--two-line", onlyIf = type == ListItemType.twoLine)
             .classes("mdl-list__item--three-line", onlyIf = type == ListItemType.threeLine)
@@ -24,7 +25,7 @@ enum class ListItemType {
     oneLine, twoLine, threeLine
 }
 
-class MDLItemElement(wrapped: Element, val type: ListItemType) : Element(wrapped) {
+class MDLItemElement(wrapped: Element, val type: ListItemType) : MDLElement(wrapped) {
     fun primaryContent(attributes: Map<String, Any> = attr) = MDLItemElement(span(attributes.classes("mdl-list__item-primary-content")), type)
     fun avatar(attributes: Map<String, Any> = attr) = MDLItemElement(i(attributes.classes("mdl-list__item-avatar")), type)
     fun icon(attributes: Map<String, Any> = attr) = MDLItemElement(i(attributes.classes("material-icons", "mdl-list__item-icon")), type)
