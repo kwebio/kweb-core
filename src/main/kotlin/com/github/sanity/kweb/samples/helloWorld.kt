0e6f74e72ff6d643ab23d7577ea3d7321d1b1ae8
Ian Clarke <github@ian.33mail.com>
2017-03-03 14:41:21 -0600

    work on docs

--- src/main/kotlin/com/github/sanity/kweb/samples/helloWorld.kt
+++ src/main/kotlin/com/github/sanity/kweb/samples/helloWorld.kt
@@ -2,6 +2,7 @@ package com.github.sanity.kweb.samples
 
 import com.github.sanity.kweb.KWeb
 import com.github.sanity.kweb.dom.element.creation.h1
+import com.github.sanity.kweb.dom.element.events.on
 import com.github.sanity.kweb.dom.element.modification.setText
 
 /**
@@ -11,7 +12,14 @@ import com.github.sanity.kweb.dom.element.modification.setText
 // [START HW]
 fun main(args: Array<String>) {
     KWeb(port = 7823) {
-        doc.body.h1().setText("Hello World!")
+        var counter = 0
+        val h1 = doc.body.h1()
+        h1.setText("Hello World!")
+
+        h1.on.click {
+            h1.setText(counter.toString())
+            counter++
+        }
     }
 }
 // [END HW]
