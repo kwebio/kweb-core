de819093563cebaf72b27352441b32e09c375ffd
Ian Clarke <github@ian.33mail.com>
2017-03-05 15:54:54 -0600

    add kotlin-reflect

--- src/main/kotlin/com/github/sanity/kweb/samples/helloWorld.kt
+++ src/main/kotlin/com/github/sanity/kweb/samples/helloWorld.kt
@@ -2,24 +2,14 @@ package com.github.sanity.kweb.samples
 
 import com.github.sanity.kweb.KWeb
 import com.github.sanity.kweb.dom.element.creation.h1
-import com.github.sanity.kweb.dom.element.events.on
 import com.github.sanity.kweb.dom.element.modification.setText
 
 /**
  * Created by ian on 3/3/17.
  */
 
-// [START HW]
 fun main(args: Array<String>) {
     KWeb(port = 7823) {
-        var counter = 0
-        val h1 = doc.body.h1()
-        h1.setText("Hello World!")
-
-        h1.on.click {
-            h1.setText(counter.toString())
-            counter++
-        }
+        doc.body.h1().setText("Hello World!")
     }
 }
-// [END HW]
