a50a5cd1f97d57603a8437efeebd6174d3cb2337
Ian Clarke <github@ian.33mail.com>
2018-07-21 15:49:54 -0500

    more todo updates

--- src/main/kotlin/io/kweb/demos/todo/todoApp.kt
+++ src/main/kotlin/io/kweb/demos/todo/todoApp.kt
@@ -11,6 +11,7 @@ import io.kweb.plugins.semanticUI.semanticUIPlugin
 import io.kweb.routing.*
 import io.kweb.state.KVar
 import io.kweb.state.persistent.*
+import io.mola.galimatias.URL
 import kotlinx.coroutines.experimental.async
 import kotlinx.coroutines.experimental.future.await
 import mu.KotlinLogging
@@ -20,25 +21,63 @@ import io.kweb.plugins.semanticUI.semantic as s
 private val logger = KotlinLogging.logger {}
 
 fun main(args: Array<String>) {
-    // Starts a web server listening on port 8091
-    Kweb(port = 8091, debug = true, plugins = listOf(semanticUIPlugin)) {
+    /** Starts a web server listening on http://localhost:8091/.
+     */
+
+    /** A simple yet flexible plugin mechanism */
+    val plugins = listOf(semanticUIPlugin)
+
+    /** Create a Kweb instance, and configure it to use the Semantic
+     * UI framework. Build a simple to-do list app.
+     * */
+    Kweb(port = 8091, debug = true, plugins = plugins) {
         doc.body.new {
 
+            /** Kweb allows you to modularize your code however suits your needs
+                best.  Here I use an extension function to renderList some common
+                outer page elements */
             pageBorderAndTitle("Todo List") {
 
-                val url = doc.receiver.url(simpleUrlParser)
+                /** A KVar is similar to an AtomicReference in the standard Java
+                    Library, but which supports the observer pattern and `map`
+                    semantics.  Here we set it to the current URL of the page.  This
+                    will update automatically the page's URL changes, and can be
+                    modified to update the page's URL,
+                    as you'll see below. */
+                val url: KVar<URL> = doc.receiver.url(simpleUrlParser)
 
+                /** s.content uses the semanticUIPlugin to use the excellent
+                    Semantic UI framework, included as a plugin above, and implemented
+                    as a convenient DSL within Kweb */
                 div(s.content).new {
-                    render(url.path[0]) { entityType ->
+
+                    /** Note how url.path[0] is itself a KVar.  Changes to firstPathElement
+                        will automatically propagate _bi-directionally_ with `url`.  This
+                        comes in very handy later. */
+                    val firstPathElement: KVar<String> = url.path[0]
+
+                    /** Renders `firstPathElement`, but - and here's the fun part - will
+                        automatically re-renderList if firstPathElement changes.  This is
+                        a simple, elegant, and yet powerful routing mechanism. */
+                    render(firstPathElement) { entityType ->
                         when (entityType) {
                             ROOT_PATH -> {
                                 val newListId = createNewList()
                                 url.path.value = listOf("lists", newListId)
                             }
                             "lists" -> {
+                                /** Renders can be nested, which means that only this
+                                    specific part of the page must be re-rendered if
+                                    url.path[1] changes, which is both incredibly convenient
+                                    for the developer, and efficient, leading to a very
+                                    responsive webapp. */
                                 render(url.path[1]) { listId ->
                                     try {
-                                        render(toVar(State.lists, listId))
+                                        /** Here we use the same render mechanism to tie DOM
+                                            state to persistent state stored in Shoebox,
+                                            Kweb's simple but powerful key-value store with
+                                            observer pattern support.  */
+                                        renderList(toVar(State.lists, listId))
                                     } catch (e: NoSuchElementException) {
                                         throw NotFoundException("Can't find list with id $listId")
                                     }
@@ -70,7 +109,7 @@ private fun createNewList(): String {
     return newListId
 }
 
-private fun ElementCreator<*>.render(list: KVar<State.List>) {
+private fun ElementCreator<*>.renderList(list: KVar<State.List>) {
     h3().text(list.map(State.List::title))
     div(s.ui.middle.aligned.divided.list).new {
         renderEach(State.itemsByList(list.value.uid)) { item ->
