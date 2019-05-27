1df023da61ca9c5fdadc0fd825025073c127ece5
Ian Clarke <ian.clarke@gmail.com>
2019-05-26 20:00:05 -0500

    clean up tests

--- src/test/kotlin/io/kweb/routing/RoutingSpec.kt
+++ src/test/kotlin/io/kweb/routing/RoutingSpec.kt
@@ -1,28 +1,27 @@
 package io.kweb.routing
 
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
 import io.kweb.state.*
-import org.amshove.kluent.shouldEqual
-import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.*
 
 /**
  * Created by ian on 4/30/17.
  */
 
-object RoutingSpec : Spek({
-    given("a simple KVar list where a subList is created") {
+object RoutingSpec : StringSpec({
+    "a simple KVar list where a subList is created" {
         val list = KVar(listOf(1, 2, 3, 4))
         val sublist = list.subList(1, 3)
-        it("should be the correct subList") {
-            sublist.value.shouldEqual(listOf(2, 3))
+        "should be the correct subList" {
+            sublist.value shouldBe listOf(2, 3)
         }
     }
-    given("a simple KVar list where a subList is created and modified") {
+    "a simple KVar list where a subList is created and modified" {
         val list = KVar(listOf(1, 2, 3, 4))
         val sublist = list.subList(1, 3)
         sublist.value = listOf(8, 9, 10)
-        it("should be the correct subList") {
-            list.value.shouldEqual(listOf(1, 8, 9, 10, 4))
+        "should be the correct subList" {
+            list.value shouldBe listOf(1, 8, 9, 10, 4)
         }
     }
 })
--- src/test/kotlin/io/kweb/state/KVarSpec.kt
+++ src/test/kotlin/io/kweb/state/KVarSpec.kt
@@ -1,24 +1,23 @@
 package io.kweb.state
 
-import org.amshove.kluent.`should be equal to`
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.specification.describe
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
 
-object KVarSpec : Spek({
-    describe("a KVar with value `dog`") {
+object KVarSpec : StringSpec({
+    "a KVar with value `dog`" {
         data class Foo(val bar : String)
         val f = Foo("dog")
         val kvf = KVar(f)
-        it("should have field with value `dog`") {
-            kvf.value.bar `should be equal to` "dog"
+        "should have field with value `dog`" {
+            kvf.value.bar shouldBe "dog"
         }
 
         val kvfp = kvf.property(Foo::bar)
 
         kvfp.value = "cat"
 
-        it ("should have modified the underlying KVar") {
-            kvf.value.bar `should be equal to` "cat"
+        "should have modified the underlying KVar" {
+            kvf.value.bar shouldBe "cat"
         }
     }
 })
