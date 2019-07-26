849b96a89e496f8079c623b49ad3d567f12fbcdf
Ian Clarke <ian.clarke@gmail.com>
2019-07-25 23:17:32 -0500

    add notNull utility to KVar

--- src/main/kotlin/io/kweb/state/KVar.kt
+++ src/main/kotlin/io/kweb/state/KVar.kt
@@ -61,3 +61,14 @@ inline fun <O : Any?, reified T : Any?> KVar<T>.property(property: KProperty1<T,
         override fun reverse(original: T, change: O): T = copyFunc.callBy(mapOf(instanceParam to original, fieldParam to change)) as T
     })
 }
+
+fun <O : Any> KVar<O?>.notNull(default : O, invertDefault : Boolean = true): KVar<O> {
+    return this.map(object : ReversableFunction<O?, O>(label = "notNull") {
+        override fun invoke(from: O?): O = from ?: default
+
+        override fun reverse(original: O?, change: O): O? = if (invertDefault) {
+            if (change != default) change else null
+        } else change
+
+    })
+}
