342a85f5c61240bd8d66e7875b6c42f95548d10a
MaanooAk <akritasak@gmail.com>
2019-09-17 09:21:39 +0300

    extended select tag creation with data to properties getters

--- src/main/kotlin/io/kweb/dom/element/creation/tags/form.kt
+++ src/main/kotlin/io/kweb/dom/element/creation/tags/form.kt
@@ -48,15 +48,21 @@ fun ElementCreator<Element>.textarea(rows : Int?, cols : Int?, required : Boolea
 }
 
 open class SelectElement(parent: Element) : ValueElement(parent, kvarUpdateEvent = "change")
-fun ElementCreator<Element>.select(options: List<Pair<String, String>>, name: String? = null, required : Boolean? = null, attributes: Map<String, Any> = attr) : SelectElement {
-    var selectElement = element("select", attributes = attributes
+fun <T : Any> ElementCreator<Element>.select(options: List<T>, optionValue: (T) -> String, optionText: (T) -> String = { it.toString() }, name: String? = null, required : Boolean? = null, attributes: Map<String, Any> = attr) : SelectElement {
+    return select(options.map {
+        optionValue(it) to optionText(it)
+    }, name, required, attributes)
+}
+fun ElementCreator<Element>.select(options: List<Pair<String, Any>>, name: String? = null, required : Boolean? = null, attributes: Map<String, Any> = attr) : SelectElement {
+    val selectElement = element("select", attributes = attributes
             .set("name", name)
             .set("required", required)
     )
 
-    var optionsCreator = ElementCreator(selectElement)
-    for (i in options) {
-        optionsCreator.element("option", mapOf("value" to i.first)).text(i.second)
+    ElementCreator(selectElement).apply {
+        for (i in options) {
+            element("option", mapOf("value" to i.first)).text(i.second.toString())
+        }
     }
 
     return SelectElement(selectElement)
