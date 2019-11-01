8e7940a6124e528de595cd9d4a20905e84021e7b
Ian Clarke <ian.clarke@gmail.com>
2019-11-01 16:20:54 -0500

    reverse static file changes as they appear to be breaking :(

--- src/main/kotlin/io/kweb/plugins/jqueryCore/JQueryCorePlugin.kt
+++ src/main/kotlin/io/kweb/plugins/jqueryCore/JQueryCorePlugin.kt
@@ -1,27 +1,20 @@
 package io.kweb.plugins.jqueryCore
 
 import io.kweb.plugins.KwebPlugin
-import io.kweb.plugins.staticFiles.ResourceFolder
-import io.kweb.plugins.staticFiles.StaticFilesPlugin
-import io.kweb.plugins.staticFiles.internalStaticFilePath
-
-private const val resourceFolder = "io/kweb/plugins/jqueryCore/static"
-private const val resourceRoute = "$internalStaticFilePath/jquery"
 
 /**
  * Created by ian on 1/9/17.
  */
-class JQueryCorePlugin : KwebPlugin(dependsOn = setOf(StaticFilesPlugin(ResourceFolder(resourceFolder), resourceRoute))) {
+class JQueryCorePlugin : KwebPlugin() {
     override fun decorate(startHead: StringBuilder, endHead: StringBuilder) {
         // Include the plugin, this is straight from the JQuery documentation
         startHead.appendln("""
         <script
-                src="$resourceRoute/jquery-3.1.1.min.js"
+                src="https://code.jquery.com/jquery-3.1.1.min.js"
         integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
         crossorigin="anonymous"></script>""".trimIndent()
         )
     }
 }
 
-// A convenience value
 val jqueryCore = JQueryCorePlugin()
