69bb8eac6b9295a8d49389beaaefb72a652a8cab
Ian Clarke <github@ian.33mail.com>
2020-03-11 09:54:59 -0500

    plugin comment improvements

--- src/main/kotlin/io/kweb/plugins/KwebPlugin.kt
+++ src/main/kotlin/io/kweb/plugins/KwebPlugin.kt
@@ -2,21 +2,33 @@ package io.kweb.plugins
 
 import io.ktor.routing.Routing
 import io.kweb.dom.element.Element
-import org.jsoup.nodes.Document
 import java.util.*
+import org.jsoup.nodes.Document as JSoupDocument
 
 abstract class KwebPlugin(val dependsOn: Set<KwebPlugin> = Collections.emptySet()) {
 
-    open fun decorate(doc : Document) {
+    /**
+     * Override this to make changes to the initial HTML document
+     */
+    open fun decorate(doc : JSoupDocument) {
 
     }
 
+    /**
+     * Override this to provide JavaScript to be executed after page creation
+     */
     open fun executeAfterPageCreation() = ""
 
+    /**
+     * Override this to add routes via KTor
+     */
     open fun appServerConfigurator(routeHandler : Routing) {
 
     }
 
+    /**
+     * Override this to execute code when a new element is created
+     */
     open fun elementCreationHook(element : Element) {
 
     }
