d3ca00e54c1fb46515910c6b3a3987f76270fdeb
Ian Clarke <github@ian.33mail.com>
2020-03-16 14:17:57 -0500

    initial rename

--- src/main/kotlin/io/kweb/dom/style/style.kt
+++
@@ -1,2 +0,0 @@
-package io.kweb.dom.style
-
--- src/main/kotlin/io/kweb/plugins/fomanticUI/SUIElement.kt
+++
@@ -1,7 +0,0 @@
-package io.kweb.plugins.fomanticUI
-
-import io.kweb.dom.element.Element
-
-fun Element.setClasses(fomanticUIClasses: FomanticUIClasses): Element {
-    return this.setClasses(fomanticUIClasses["class"].toString())
-}
