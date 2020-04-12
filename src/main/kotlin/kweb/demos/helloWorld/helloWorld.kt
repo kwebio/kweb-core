5f0ce54a46b7c6e2be524bad545586bc083dcce2
Ian Clarke <ian.clarke@gmail.com>
2020-04-11 20:24:21 -0500

    hello world updates

--- src/main/kotlin/kweb/demos/helloWorld/helloWorld.kt
+++ src/main/kotlin/kweb/demos/helloWorld/helloWorld.kt
@@ -1,13 +1,23 @@
 package kweb.demos.helloWorld
 
-import kweb.Kweb
-import kweb.h1
-import kweb.new
+import kweb.*
 
-fun main() {
+fun helloWorld1() {
     Kweb(port = 16097, buildPage = {
         doc.body.new {
             h1().text("Hello World!")
         }
     })
+}
+
+fun helloWorld2() {
+    Kweb(port = 16097) {
+        doc.body.new {
+            ul().new {
+                for (x in 1..5) {
+                    li().text("Hello World $x!")
+                }
+            }
+        }
+    }
 }
