75f617756ca12e953b3738c6fcdea992acf76f58
Alexandros Papageorgiou <alkoclick@gmail.com>
2020-05-08 21:58:38 +0200

    #133 Documentation can reduce frustration of migration
    and false expectation :)

--- src/main/kotlin/kweb/KtorFeature.kt
+++ src/main/kotlin/kweb/KtorFeature.kt
@@ -6,6 +6,7 @@ import io.ktor.application.call
 import io.ktor.application.feature
 import io.ktor.routing.get
 import io.ktor.routing.routing
+import kweb.routing.PathReceiver
 
 /**
  * Kweb normally calls this function on initialization, if you provide a buildpage,
@@ -13,8 +14,10 @@ import io.ktor.routing.routing
  *
  * If you are using Ktor, this is an easy way to migrate from the deprecated buildPage design
  *
- * Kweb will still do its internal routing properly, e.g if you use `path()` in your buildPage,
- * but it will also respect Ktor defined routes ^-^
+ * Kweb will still do its internal routing properly, e.g if you use `path()` in your buildPage. ^-^
+ *
+ * Nevertheless, please be very careful and avoid using both models in parallel as they
+ * have lots of subtle differences and you're bound to hit something!
  */
 fun Application.installKwebOnRemainingRoutes(buildPage: WebBrowser.() -> Unit) {
     routing {
@@ -33,12 +36,28 @@ suspend fun ApplicationCall.respondKweb(buildPage: WebBrowser.() -> Unit) =
     application.feature(Kweb).respondKweb(this, buildPage)
 
 /**
+ * If you were previously using Kweb routes and you want to switch to Ktor, this is for you.
+ * You can copy your previous path receivers and they will be run the same way as before,
+ *
+ * EXCEPT FOR THE PARAMETERS
+ *
+ * Note that parameter handling is no longer done by Kweb, you'll have to use the Ktor
+ * [io.ktor.http.Parameters] functions
+ *
+ * @param pathReceiver the receiver to execute on this path
+ */
+suspend fun ApplicationCall.respondKwebRender(pathReceiver: PathReceiver) =
+    respondKweb {
+        doc.body.new { span().new { pathReceiver(this, emptyMap()) } }
+    }
+
+/**
  * Use this if you are on Ktor and migrating out of the deprecated buildPage design
  */
 const val buildPageReplacementCode = """
     routing {
         get("/{visitedUrl...}") {
-            call.respondKweb { 
+            call.respondKwebRender { 
                 buildPage
             }
         }
