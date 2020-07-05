3eff78da9a588cbad35084cb3daf05d665e70a1f
Ian Clarke <ian.clarke@gmail.com>
2020-07-04 22:35:00 -0500

    improve documentation per #154

--- src/main/kotlin/kweb/client/HttpRequestInfo.kt
+++ src/main/kotlin/kweb/client/HttpRequestInfo.kt
@@ -4,9 +4,13 @@ import io.ktor.features.origin
 import io.ktor.request.ApplicationRequest
 
 /**
- * @param request This is the raw ApplicationRequest object to [Ktor](https://github.com/Kotlin/ktor), the HTTP
- *                library used by Kweb.  It can be used to read various information about the inbound HTTP request,
- *                however you should use properties of [HttpRequestInfo] directly instead if possible.
+ * @param request This is the raw [ApplicationRequest](https://api.ktor.io/1.3.2/io.ktor.request/-application-request/index.html)
+ *                provided by [Ktor](https://github.com/Kotlin/ktor), the HTTP transport library used by Kweb.  It can be used
+ *                to read various information about the inbound HTTP request, however you should use properties of
+ *                [HttpRequestInfo] directly instead if possible.
+ *
+ *                Note that values in `ApplicationRequest` can change during a connection, see [issue #154])(https://github.com/kwebio/kweb-core/issues/154).
+ *
  */
 data class HttpRequestInfo(val request: ApplicationRequest) {
 
