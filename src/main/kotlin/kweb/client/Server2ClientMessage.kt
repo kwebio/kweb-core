ea5067ec8fd1128f55a41f4ea72f4f9aa9836b44
Derek Briggs <contact@derekbriggs.xyz>
2021-03-02 18:33:02 -0500

    Comment on Server2ClientMessage.kt

--- src/main/kotlin/kweb/client/Server2ClientMessage.kt
+++ src/main/kotlin/kweb/client/Server2ClientMessage.kt
@@ -3,9 +3,11 @@ package kweb.client
 data class Server2ClientMessage(
         val yourId: String,
         val debugToken: String?,
-        val jsId: Int? = null,
-        val js: String? = null,
-        val parameters: String? = null,
-        val callbackId: Int? = null,
-        val arguments: List<Any?>? = null
+        val jsId: Int? = null, //the id used to store or receive a js function in cache
+        val js: String? = null, //the js function
+        val parameters: String? = null, //null if we are executing a cached function
+        val callbackId: Int? = null, //null if we are executing a function without a callback
+        val arguments: List<Any?>? = null //null if we are executing a function with no arguments
 )
+
+//parameters is a comma separated string of parameters for the js function
