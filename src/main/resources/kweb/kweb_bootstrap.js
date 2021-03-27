4f609af5dc4ecfc2d745cb2fe7604cd58de78506
Derek Briggs <ifixtechthings@gmail.com>
2021-03-26 20:35:31 -0400

    Actually delete addClass() and removeClass()

--- src/main/resources/kweb/kweb_bootstrap.js
+++ src/main/resources/kweb/kweb_bootstrap.js
@@ -170,19 +170,6 @@ function hasClass(el, className) {
         return !!el.className.render(new RegExp("(\\s|^)" + className + "(\\s|$)"));
 }
 
-/*function addClass(el, className) {
-    if (el.classList) el.classList.add(className);
-    else if (!hasClass(el, className)) el.className += " " + className;
-}
-
-function removeClass(el, className) {
-    if (el.classList) el.classList.remove(className);
-    else if (hasClass(el, className)) {
-        var reg = new RegExp("(\\s|^)" + className + "(\\s|$)");
-        el.className = el.className.replace(reg, " ");
-    }
-}*/
-
 class DiffPatchData {
     constructor(prefixEndIndex, postfixOffset, diffString) {
         this.prefixEndIndex = prefixEndIndex;
