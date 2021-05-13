7fa1271546f66575eb52ab6545dff8ae3a98bd7c
Ian Clarke <ian.clarke@gmail.com>
2021-05-13 13:18:14 -0500

    use MemoryStore for ToDo demo

--- src/main/kotlin/kweb/demos/todo/ToDoState.kt
+++ src/main/kotlin/kweb/demos/todo/ToDoState.kt
@@ -3,6 +3,7 @@ package kweb.demos.todo
 import kotlinx.serialization.Serializable
 import kweb.shoebox.Shoebox
 import kweb.shoebox.stores.DirectoryStore
+import kweb.shoebox.stores.MemoryStore
 import java.nio.file.Files
 import java.nio.file.Path
 
@@ -10,20 +11,14 @@ import java.nio.file.Path
  * Stores all persistent state for this app in a directory, creating it if necessary
  */
 
-class ToDoState(dir: Path) {
-    init {
-        if (Files.notExists(dir)) {
-            Files.createDirectory(dir)
-        }
-    }
-
+class ToDoState {
     @Serializable data class List(val uid: String, val title: String)
 
     @Serializable data class Item(val uid: String, val created : Long, val listUid: String, val text: String)
 
-    val lists = Shoebox(DirectoryStore(dir.resolve("lists"), List.serializer()))
+    val lists = Shoebox(MemoryStore<List>())
 
-    val items = Shoebox(DirectoryStore(dir.resolve("items"), Item.serializer()))
+    val items = Shoebox(MemoryStore<Item>())
 
     private val itemsByList = items.view("itemsByList", Item::listUid)
 
