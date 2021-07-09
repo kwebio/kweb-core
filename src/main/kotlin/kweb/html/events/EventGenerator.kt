781cd606fc373863f1f42685d769b50b89177ee5
Ian Clarke <sanity@users.noreply.github.com>
2021-07-09 17:12:07 -0400

    add TODO

--- src/main/kotlin/kweb/html/events/EventGenerator.kt
+++ src/main/kotlin/kweb/html/events/EventGenerator.kt
@@ -7,6 +7,10 @@ import java.util.*
 /**
  * Something that event listeners can be attached to, such as an [kweb.Element] or a [kweb.html.Document]
  */
+
+// TODO: Code for both event listener functions should be generalized
+//       and lifted into this class, paying close attention to
+//       any differences between the different implementations.
 interface EventGenerator<T> {
     val browser: WebBrowser
     fun addImmediateEventCode(eventName: String, jsCode: String)
