3e6e59580a9b25576560b91350325da7a8713af8
Derek Briggs <contact@derekbriggs.xyz>
2022-01-26 19:38:32 -0500

    Begin reworking RenderEachTests

--- src/test/kotlin/kweb/state/render/RenderEachTest.kt
+++ src/test/kotlin/kweb/state/render/RenderEachTest.kt
@@ -1,14 +1,57 @@
 package kweb.state.render
 
+import io.github.bonigarcia.seljup.Arguments
+import io.github.bonigarcia.seljup.Options
+import io.github.bonigarcia.seljup.SeleniumExtension
+import io.kotlintest.shouldBe
 import kweb.*
+import kweb.demos.todo.TodoApp
 import kweb.state.KVal
 import kweb.state.KVar
 import kweb.state.ObservableList
 import kweb.state.renderEachWIP
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.openqa.selenium.By
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
+import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.firefox.FirefoxOptions
 
-class RenderEachTest {
+@ExtendWith(SeleniumExtension::class)
+class RenderEachTest(@Arguments("--headless") private var driver: WebDriver) {
 
+    /*companion object {
+        private lateinit var testSite: RenderEachSite
+
+        @JvmStatic
+        @BeforeAll
+        fun setupServer() {
+            testSite = RenderEachSite()
+        }
+
+        @JvmStatic
+        @AfterAll
+        fun teardownServer() {
+            testSite.server.close()
+            todoKweb.server.close()
+        }
+
+        //selenium-jupiter will automatically fall back if the first browser it tries doesn't work
+        //https://bonigarcia.github.io/selenium-jupiter/#generic-driver
+        @Options
+        var chromeOptions = ChromeOptions().apply {
+            setHeadless(true)
+        }
+
+        @Options
+        var firefoxOptions = FirefoxOptions().apply {
+            setHeadless(true)
+        }
+    }
+*/
     @Test
     fun prependItemTest() {
         val animals = ObservableList(mutableListOf("Dog", "Cat", "Bear", "Horse"))
@@ -27,6 +70,8 @@ class RenderEachTest {
                     }
             }
         })
+
+
         Thread.sleep(20000)
         println(animals.getItems())
     }
@@ -142,4 +187,46 @@ class RenderEachTest {
         Thread.sleep(100000)
     }
 
+    @Test
+    fun ClearItemsTest() {
+        val animals = ObservableList(mutableListOf("Dog", "Cat", "Bear", "Moose", "Horse"))
+
+        val server = Kweb(port = 1234, buildPage = {
+            doc.body.new {
+                renderEachWIP(animals) { animal ->
+                    div().new {
+                        h1().text(animal)
+                    }
+                }
+                button()
+                    .text("Clear items")
+                    .on.click {
+                        animals.clear()
+                        println("Animals isEmpty: ${animals.isEmpty()}")
+                    }
+            }
+        })
+        driver.get("http://localhost:1234")
+        val label = driver.findElements<WebElement>(By.tagName("h1"))
+        label[0].text shouldBe("Dog")
+        server.close()
+    }
 }
+
+/*
+class RenderEachSite(private val driver: WebDriver) {
+    val server = Kweb(port = 1234, buildPage = {
+        doc.body.new {
+            renderEachWIP(animals) { animal ->
+                div().new {
+                    h1().text(animal)
+                }
+            }
+            button()
+                .text("Prepend Moose to list")
+                .on.click {
+                    animals.add(0, "Moose")
+                }
+        }
+    })
+}*/
