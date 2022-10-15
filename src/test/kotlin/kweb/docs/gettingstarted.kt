19d9af43299a7f086f91f2ac9ea63c7f8db2405e
Ian Clarke <ian.clarke@gmail.com>
2022-10-15 15:18:22 -0500

    more doc improvements

--- src/test/kotlin/kweb/docs/gettingstarted.kt
+++ src/test/kotlin/kweb/docs/gettingstarted.kt
@@ -12,42 +12,42 @@ fun main() {
 }
 // ANCHOR_END: hello_world
 
-// ANCHOR: hello_world_2
 fun helloWorld2() {
-    Kweb(port = 16097) {
-        doc.body {
-            ul {
-                for (x in 1..5) {
-                    li().text("Hello World $x!")
-                }
+// ANCHOR: hello_world_2
+Kweb(port = 16097) {
+    doc.body {
+        ul {
+            for (x in 1..5) {
+                li().text("Hello World $x!")
             }
         }
     }
 }
 // ANCHOR_END: hello_world_2
+}
 
-// ANCHOR: hello_world_3
 fun helloWorld3() {
-    Kweb(port = 16097) {
-        doc.body {
-            ul {
-                // We can modify the UL element we just created in an element {} block
-                element {
-                    // Here we add a CSS class to the UL element
-                    classes("list")
-                }
-                // We can define functions as an extension to ElementCreator allowing us to use it
-                // within the DSL
-                fun ElementCreator<ULElement>.createMessage(x: Int) {
-                    li().text("Hello World $x!")
-                }
+// ANCHOR: hello_world_3
+Kweb(port = 16097) {
+    doc.body {
+        ul {
+            // We can modify the UL element we just created in an element {} block
+            element {
+                // Here we add a CSS class to the UL element
+                classes("list")
+            }
+            // We can define functions as an extension to ElementCreator allowing us to use it
+            // within the DSL
+            fun ElementCreator<ULElement>.createMessage(x: Int) {
+                li().text("Hello World $x!")
+            }
 
-                for (x in 1..5) {
-                    createMessage(x)
-                }
+            for (x in 1..5) {
+                createMessage(x)
             }
         }
     }
 }
 // ANCHOR_END: hello_world_3
+}
 
