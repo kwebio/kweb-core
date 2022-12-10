4de8d6df500023c2d8204855c384bbe133e321dd
Ian Clarke <ian.clarke@gmail.com>
2022-12-10 10:53:45 -0600

    remove shadow plugin, not needed

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     buildsrc.conventions.`kotlin-jvm`
     buildsrc.conventions.`maven-publish`
     id("org.jetbrains.dokka") version "1.7.20"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
     id("com.github.ben-manes.versions") version "0.44.0"
     kotlin("plugin.serialization")
 
