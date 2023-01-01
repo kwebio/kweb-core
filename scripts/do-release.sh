5582023341e2419d6e21a652661ba6a1c217d15c
Ian Clarke <ian@freenet.org>
2023-01-01 12:52:08 +0000

    remove old script

--- scripts/do-release.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-CURRENT_RELEASE=$(gh release list -L 1 | cut -f3)
-echo "Previous Release: $CURRENT_RELEASE"
-read -pr "New Release: " NEW_RELEASE
-gh release create "$NEW_RELEASE" -p
